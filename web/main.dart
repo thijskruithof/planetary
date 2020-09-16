f7d3d78c461f30e012f4ec6a7c2ec39cfdb97b2f
Thijs Kruithof <thijskruithof@gmail.com>
2020-09-16 09:42:52 +0200

    Setting up some simple webgl rendering in a dart webapp.

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,31 @@
 import 'dart:html';
+import 'dart:web_gl';
+
+CanvasElement canvas = document.getElementById('planetaryCanvas');
+RenderingContext gl;
+
+void initGL() {
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.viewport(0, 0, canvas.width, canvas.height);
+}
+
+void render(num deltaTime) {
+  gl.clear(0x00004000);
+
+  // draw
+//   gl.drawArrays(RenderingContext.TRIANGLES, 0, 3);
+
+  // redraw when ready
+  window.animationFrame.then(render);
+}
+
+// https://gist.github.com/m-decoster/ec44495badb54c26bb1c
 
 void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+  gl = canvas.getContext3d();
+  assert(gl != null);
+
+  initGL();
+
+  window.animationFrame.then(render);
 }
